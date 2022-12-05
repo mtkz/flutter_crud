ec2a6d9f7e8311fce54121a611a116acc5f8bd81
mohammad tahourian <mhdtahourian@gmail.com>
2022-12-05 17:29:25 +0330

    profile ui completed

--- lib/ui/screens/profile/profile.dart
+++ lib/ui/screens/profile/profile.dart
@@ -7,6 +7,7 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,8 +17,73 @@ class ProfileScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(ThemeConstants.themePadding),
-        child: Column(
-          children: [],
+        child: SizedBox(
+          width: size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 45,
+              ),
+              Text('Mohammad Tahourian',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6!
+                      .apply(fontWeightDelta: 3)),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('mhdtahoirian@gmail.com'),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      borderRadius: BorderRadius.circular(
+                          ThemeConstants.themeBorderRadius),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10, color: Colors.black.withOpacity(.1))
+                      ]),
+                  width: size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(
+                      ThemeConstants.themePadding,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Edit Profile'),
+                        Icon(Icons.arrow_forward_ios_rounded)
+                      ],
+                    ),
+                  )),
+                  const SizedBox(height: 12,),
+                  Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      borderRadius: BorderRadius.circular(
+                          ThemeConstants.themeBorderRadius),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 6, color: Colors.black.withOpacity(.1))
+                      ]),
+                  width: size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(
+                      ThemeConstants.themePadding,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Logout' , style: TextStyle(color: Color(0xfff44336)),),
+                        Icon(Icons.arrow_forward_ios_rounded)
+                      ],
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
